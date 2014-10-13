31ef778d49b77a6c665d2850ee3fd1df75275271
hogesuke <miyado@gmail.com>
2014-10-13 19:01:49 +0900

    angular-routeを追加

--- www/js/app.js
+++ www/js/app.js
@@ -1,19 +1,19 @@
 'use strict';
 
 var videosApp = angular.module('videosApp', [
-//	'ngRoute',
+	'ngRoute',
 	'stockVideosControllers'
 ]);
 
-//videosApp.config(['$routeProvider',
-//	function($routeProvider) {
-//		$routeProvider.
-//			when('/', {
-//				templateUrl: '../../templates/index.html',
-//				controller: 'VideoListController'
-//			}).
-//			otherwise({
-//				redirectTo: '/videos/list'
-//			});
-//	}
-//]);
+videosApp.config(['$routeProvider',
+	function($routeProvider) {
+		$routeProvider.
+			when('/', {
+				templateUrl: '../../templates/index.html',
+				controller: 'VideoListController'
+			}).
+			otherwise({
+				redirectTo: '/videos/list'
+			});
+	}
+]);

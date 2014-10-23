44e08c8db49adc3875041ae9c9be22985955b7c5
hogesuke <miyado@gmail.com>
2014-10-24 07:22:45 +0900

    htmlのファイル名、配置場所を変更

--- www/js/app.js
+++ www/js/app.js
@@ -9,20 +9,11 @@ videosApp.config(['$routeProvider',
 	function($routeProvider) {
 		$routeProvider.
 			when('/top', {
-				templateUrl: '../templates/top.tmpl.html',
-				controller: 'VideoListController'
+				templateUrl: '../templates/top.tmpl.html'
 			}).
 			when('/videos/:videoId', {
 				templateUrl: '../templates/video.tmpl.html',
 				controller: 'VideoDetailController'
-			}).
-			when('/my/videos', {
-				templateUrl: '../templates/myVideos.tmpl.html',
-				controller: 'MyVideoListController'
-			}).
-			when('/my/contributors', {
-				templateUrl: '../templates/myContributors.tmpl.html',
-				controller: 'AddMyContributorController'
 			}).
 			otherwise({
 				redirectTo: '/top'

f7c54ba2637976a6bb12fc51d93dcb74b71b67b6
hogesuke <miyado@gmail.com>
2016-10-30 11:46:44 +0900

    広告の入れ替え

--- www/js/services/ad-service.js
+++ www/js/services/ad-service.js
@@ -1,85 +1,55 @@
 angular.module('videosApp').factory('AdService', [function() {
   return {
     ads: [
-      {
-        anchorUrl: 'https://px.a8.net/svt/ejp?a8mat=2NTB30+A1ZM4I+3F2A+60OXD&cid=a13032385546',
-        imgUrl   : 'https://www20.a8.net/svt/bgt?aid=160930908608&wid=003&eno=01&mid=s00000015949001011000&mc=1',
-        secretUrl: 'https://www14.a8.net/0.gif?a8mat=2NTB30+A1ZM4I+3F2A+60OXD'
-      },
-      {
-        anchorUrl: 'https://px.a8.net/svt/ejp?a8mat=2NTB30+ESV0QQ+3K9S+5ZEMP',
-        imgUrl   : 'https://www28.a8.net/svt/bgt?aid=160930908895&wid=003&eno=01&mid=s00000016624001005000&mc=1',
-        secretUrl: 'https://www14.a8.net/0.gif?a8mat=2NTB30+ESV0QQ+3K9S+5ZEMP'
-      },
-      {
-        anchorUrl: 'https://px.a8.net/svt/ejp?a8mat=2NV810+CI3RNM+MZI+3N1AJ5',
-        imgUrl   : 'https://www28.a8.net/svt/bgt?aid=161020260756&wid=003&eno=01&mid=s00000002979022009000&mc=1',
-        secretUrl: 'https://www19.a8.net/0.gif?a8mat=2NV810+CI3RNM+MZI+3N1AJ5'
-      },
-      {
-        anchorUrl: 'https://px.a8.net/svt/ejp?a8mat=2NV810+CIP79E+MZI+3T0HG1',
-        imgUrl   : 'https://www23.a8.net/svt/bgt?aid=161020260757&wid=003&eno=01&mid=s00000002979023013000&mc=1',
-        secretUrl: 'https://www15.a8.net/0.gif?a8mat=2NV810+CIP79E+MZI+3T0HG1'
-      },
-      {
-        anchorUrl: 'https://px.a8.net/svt/ejp?a8mat=2NV810+CJW2GY+33DG+61JSH',
-        imgUrl   : 'https://www29.a8.net/svt/bgt?aid=161020260759&wid=003&eno=01&mid=s00000014434001015000&mc=1',
-        secretUrl: 'https://www19.a8.net/0.gif?a8mat=2NV810+CJW2GY+33DG+61JSH'
-      },
-      {
-        anchorUrl: 'https://px.a8.net/svt/ejp?a8mat=2NV810+CCQV7M+2UOE+1HN5LT',
-        imgUrl   : 'https://www21.a8.net/svt/bgt?aid=161020260747&wid=003&eno=01&mid=s00000013307009010000&mc=1',
-        secretUrl: 'https://www17.a8.net/0.gif?a8mat=2NV810+CCQV7M+2UOE+1HN5LT'
-      },
-      {
+      { // DMM FX
         anchorUrl: 'https://px.a8.net/svt/ejp?a8mat=2NVCQ9+2IHY9U+1WP2+63H8H',
         imgUrl   : 'https://www26.a8.net/svt/bgt?aid=161026353152&wid=003&eno=01&mid=s00000008903001024000&mc=1',
         secretUrl: 'https://www10.a8.net/0.gif?a8mat=2NVCQ9+2IHY9U+1WP2+63H8H'
       },
-      {
-        anchorUrl: 'https://px.a8.net/svt/ejp?a8mat=2NVCQ9+23M45E+38GS+60WN5',
-        imgUrl   : 'https://www28.a8.net/svt/bgt?aid=161026353127&wid=003&eno=01&mid=s00000015094001012000&mc=1',
-        secretUrl: 'https://www16.a8.net/0.gif?a8mat=2NVCQ9+23M45E+38GS+60WN5'
-      },
-      {
-        anchorUrl: 'https://px.a8.net/svt/ejp?a8mat=2NVCQ9+20MY4I+1B26+ZQV5T',
-        imgUrl   : 'https://www27.a8.net/svt/bgt?aid=161026353122&wid=003&eno=01&mid=s00000006099006004000&mc=1',
-        secretUrl: 'https://www18.a8.net/0.gif?a8mat=2NVCQ9+20MY4I+1B26+ZQV5T'
-      },
-      {
+      { // PCMAX
         anchorUrl: 'https://px.a8.net/svt/ejp?a8mat=2NVCQ9+1RPG1U+YQK+6JC81',
         imgUrl   : 'https://www26.a8.net/svt/bgt?aid=161026353107&wid=003&eno=01&mid=s00000004502001098000&mc=1',
         secretUrl: 'https://www19.a8.net/0.gif?a8mat=2NVCQ9+1RPG1U+YQK+6JC81'
       },
-      {
-        anchorUrl: 'https://px.a8.net/svt/ejp?a8mat=2NVEAB+86PMRM+2KX0+1HQ5MP',
-        imgUrl   : 'https://www26.a8.net/svt/bgt?aid=161028371495&wid=003&eno=01&mid=s00000012042009024000&mc=1',
-        secretUrl: 'https://www11.a8.net/0.gif?a8mat=2NVEAB+86PMRM+2KX0+1HQ5MP'
-      },
-      {
+      { // ドスパラ スティックPC
         anchorUrl: 'https://px.a8.net/svt/ejp?a8mat=2NVEAB+85IRK2+NTS+NVHCX',
         imgUrl   : 'https://www25.a8.net/svt/bgt?aid=161028371493&wid=003&eno=01&mid=s00000003088004010000&mc=1',
         secretUrl: 'https://www11.a8.net/0.gif?a8mat=2NVEAB+85IRK2+NTS+NVHCX'
       },
-      {
+      { // ドスパラ BTO
+        anchorUrl: 'https://px.a8.net/svt/ejp?a8mat=2C0RZT+BRB9FM+NTS+72EY9',
+        imgUrl   : 'https://www26.a8.net/svt/bgt?aid=141124025711&wid=003&eno=01&mid=s00000003088001187000&mc=1',
+        secretUrl: 'https://www17.a8.net/0.gif?a8mat=2C0RZT+BRB9FM+NTS+72EY9'
+      },
+      { // パンダネット
         anchorUrl: 'https://px.a8.net/svt/ejp?a8mat=2NVF2Q+5VCY2Q+8BQ+5YJRL',
         imgUrl   : 'https://www27.a8.net/svt/bgt?aid=161029394355&wid=003&eno=01&mid=s00000001079001001000&mc=1',
         secretUrl: 'https://www19.a8.net/0.gif?a8mat=2NVF2Q+5VCY2Q+8BQ+5YJRL'
       },
-      {
+      { // 麻雀ゲーム
         anchorUrl: 'https://px.a8.net/svt/ejp?a8mat=2NVF2Q+5OT6F6+1LX0+614CX',
         imgUrl   : 'https://www25.a8.net/svt/bgt?aid=161029394344&wid=003&eno=01&mid=s00000007506001013000&mc=1',
         secretUrl: 'https://www16.a8.net/0.gif?a8mat=2NVF2Q+5OT6F6+1LX0+614CX'
       },
-      {
+      { // mobage
         anchorUrl: 'https://px.a8.net/svt/ejp?a8mat=2NVF2Q+5MFG02+2NPU+5ZU29',
         imgUrl   : 'https://www24.a8.net/svt/bgt?aid=161029394340&wid=003&eno=01&mid=s00000012405001007000&mc=1',
         secretUrl: 'https://www10.a8.net/0.gif?a8mat=2NVF2Q+5MFG02+2NPU+5ZU29'
       },
-      {
+      { // mobage
         anchorUrl: 'https://px.a8.net/svt/ejp?a8mat=2NVF2Q+5MFG02+2NPU+6IP2P',
         imgUrl   : 'https://www27.a8.net/svt/bgt?aid=161029394340&wid=003&eno=01&mid=s00000012405001095000&mc=1',
         secretUrl: 'https://www19.a8.net/0.gif?a8mat=2NVF2Q+5MFG02+2NPU+6IP2P'
+      },
+      { // ペイントツール
+        anchorUrl: 'https://px.a8.net/svt/ejp?a8mat=2NVFUM+FJNIYQ+3AP2+60H7L',
+        imgUrl   : 'https://www27.a8.net/svt/bgt?aid=161030398940&wid=003&eno=01&mid=s00000015383001010000&mc=1',
+        secretUrl: 'https://www11.a8.net/0.gif?a8mat=2NVFUM+FJNIYQ+3AP2+60H7L'
+      },
+      { // BTOパソコン FRONTIER
+        anchorUrl: 'https://px.a8.net/svt/ejp?a8mat=2NVFUM+F5D4G2+3KX4+639IP',
+        imgUrl   : 'https://www26.a8.net/svt/bgt?aid=161030398916&wid=003&eno=01&mid=s00000016708001023000&mc=1',
+        secretUrl: 'https://www16.a8.net/0.gif?a8mat=2NVFUM+F5D4G2+3KX4+639IP'
       }
     ],
     pick: function () {
